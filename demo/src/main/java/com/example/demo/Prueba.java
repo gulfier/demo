deb901252b019961e0750d55d0065b7ba2bca326
GULFIER LEONEL LOPEZ VAZQUEZ <gllopezv@ad.elektra.com.mx>
2020-12-21 07:41:42 -0600

    dps3333

---
+++ demo/src/main/java/com/example/demo/Prueba.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class Prueba {
+	
+	
+
+	  @RequestMapping("/hello")
+	  public String  getByCode()
+	  {
+	  return "Hola";
+	  }
+
+}
